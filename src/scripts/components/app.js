817377bbfa3ea3c08287c5e109a15e49537a4ada
Aruna <myruala@gmail.com>
2019-02-25 15:01:43 +0530

    adaptive fixes

--- src/scripts/components/app.js
+++ src/scripts/components/app.js
@@ -19,19 +19,29 @@ $(document).ready(function () {
                     {
                         breakpoint: 992,
                         settings: {
-                            slidesToShow: 4
+                            slidesToShow: 4,
+                            dots: false
                         }
                     },
                     {
                         breakpoint: 768,
                         settings: {
-                            slidesToShow: 3
+                            slidesToShow: 3,
+                            dots: false
                         }
                     },
                     {
                         breakpoint: 576,
                         settings: {
-                            slidesToShow: 2
+                            slidesToShow: 2,
+                            dots: false
+                        }
+                    },
+                    {
+                        breakpoint: 410,
+                        settings: {
+                            slidesToShow: 1,
+                            dots: false
                         }
                     }
                 ]
