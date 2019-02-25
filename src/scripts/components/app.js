b27d792d4c7511482fc00a448b582b9739f8e417
Aruna <myruala@gmail.com>
2019-02-25 14:42:58 +0530

    PK version

--- src/scripts/components/app.js
+++ src/scripts/components/app.js
@@ -1,33 +1,51 @@
 $(document).ready(function () {
 
+    //Sliders
     (function () {
-        const $slider = $('.brands-slider');
+        //Brands slider
+        (function () {
+            const $slider = $('.brands-slider');
 
-        $slider.slick({
-            dots: true,
-            slidesToShow: 6,
-            responsive: [
-                {
-                    breakpoint: 992,
-                    settings: {
-                        slidesToShow: 4
+            $slider.slick({
+                dots: true,
+                slidesToShow: 6,
+                responsive: [
+                    {
+                        breakpoint: 1200,
+                        settings: {
+                            slidesToShow: 5
+                        }
+                    },
+                    {
+                        breakpoint: 992,
+                        settings: {
+                            slidesToShow: 4
+                        }
+                    },
+                    {
+                        breakpoint: 768,
+                        settings: {
+                            slidesToShow: 3
+                        }
+                    },
+                    {
+                        breakpoint: 576,
+                        settings: {
+                            slidesToShow: 2
+                        }
                     }
-                },
-                {
-                    breakpoint: 768,
-                    settings: {
-                        slidesToShow: 3
-                    }
-                },
-                {
-                    breakpoint: 576,
-                    settings: {
-                        slidesToShow: 2
-                    }
-                }
-            ]
-        });
+                ]
+            });
 
+        })();
+
+        //preview carousel
+        (function () {
+            const $slider = $('.preview-carousel');
+
+            $slider.slick();
+
+        })();
     })();
 
 
